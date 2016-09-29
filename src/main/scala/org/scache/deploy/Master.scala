1b1a63aff648cc639a1483c3a585bec47aaea2bd
frankfzw <frankfzw@gmail.com>
2016-09-29 15:46:15 +0800

    add client

--- src/main/scala/org/scache/deploy/Master.scala
+++ src/main/scala/org/scache/deploy/Master.scala
@@ -4,24 +4,28 @@ package org.scache.deploy
  * Created by frankfzw on 16-8-4.
  */
 
-import org.scache.deploy.DeployMessages.Heartbeat
+import org.scache.deploy.DeployMessages.{RegisterClient, Heartbeat}
 import org.scache.network.netty.NettyBlockTransferService
 import org.scache.scheduler.LiveListenerBus
 import org.scache.storage.memory.{UnifiedMemoryManager, StaticMemoryManager, MemoryManager}
 import org.scache.{MapOutputTrackerMasterEndpoint, MapOutputTracker, MapOutputTrackerMaster}
-import org.scache.rpc.{RpcEndpoint, ThreadSafeRpcEndpoint, RpcEndpointRef, RpcEnv}
+import org.scache.rpc._
 import org.scache.serializer.{SerializerManager, JavaSerializer}
 import org.scache.storage.{BlockManager, BlockManagerMasterEndpoint, BlockManagerMaster}
-import org.scache.util.{RpcUtils, Logging, ScacheConf}
+import org.scache.util.{IdGenerator, RpcUtils, Logging, ScacheConf}
+
+import scala.collection.mutable
 
 private class Master(
     val rpcEnv: RpcEnv,
     val hostname: String,
-    val port: Int,
     conf: ScacheConf,
     isDriver: Boolean = true,
     isLocal: Boolean) extends ThreadSafeRpcEndpoint with Logging {
   val numUsableCores = conf.getInt("scache.cores", 1)
+
+  val clientIdToInfo: mutable.HashMap[Int, ClientInfo] = new mutable.HashMap[Int, ClientInfo]()
+  val hostnameToClientId: mutable.HashMap[String, Int] = new mutable.HashMap[String, Int]()
 
 
   conf.set("scache.master.port", rpcEnv.address.port.toString)
@@ -55,6 +59,32 @@ private class Master(
   override def receive: PartialFunction[Any, Unit] = {
     case Heartbeat(id, rpcRef) =>
       logInfo(s"Receive heartbeat from ${id}: ${rpcRef}")
+    case RegisterClient(hostname, port, ref) =>
+      if (hostnameToClientId.contains(hostname)) {
+        logWarning(s"The client ${hostname}:${hostnameToClientId(hostname)} has registered again")
+        clientIdToInfo.remove(hostnameToClientId(hostname))
+      }
+      val clientId = Master.EXECUTOR_ID_GENERATOR.next
+      val info = new ClientInfo(clientId, hostname, port, ref)
+      hostnameToClientId.getOrElseUpdate(hostname, clientId)
+      clientIdToInfo.getOrElseUpdate(clientId, info)
+      logInfo(s"Register client ${hostname} with id ${clientId}")
+    case _ =>
+      logError("Empty message received !")
+  }
+
+  override def receiveAndReply(context: RpcCallContext): PartialFunction[Any, Unit] = {
+    case RegisterClient(hostname, port, ref) =>
+      if (hostnameToClientId.contains(hostname)) {
+        logWarning(s"The client ${hostname}:${hostnameToClientId(hostname)} has registered again")
+        clientIdToInfo.remove(hostnameToClientId(hostname))
+      }
+      val clientId = Master.EXECUTOR_ID_GENERATOR.next
+      val info = new ClientInfo(clientId, hostname, port, ref)
+      hostnameToClientId.getOrElseUpdate(hostname, clientId)
+      clientIdToInfo.getOrElseUpdate(clientId, info)
+      logInfo(s"Register client ${hostname} with id ${clientId}")
+      context.reply(clientId)
     case _ =>
       logError("Empty message received !")
   }
@@ -63,18 +93,23 @@ private class Master(
 }
 
 object Master extends Logging {
+  private val EXECUTOR_ID_GENERATOR = new IdGenerator
 
   def main(args: Array[String]): Unit = {
     logInfo("Start Master")
     val conf = new ScacheConf()
     val SYSTEM_NAME = "scache.master"
-    val arguements = new MasterArguments(args, conf)
-    val rpcEnv = RpcEnv.create(SYSTEM_NAME, arguements.host, arguements.port, conf)
+    val arguments = new MasterArguments(args, conf)
+    val rpcEnv = RpcEnv.create(SYSTEM_NAME, arguments.host, arguments.port, conf)
     val masterEndpoint = rpcEnv.setupEndpoint("Master",
-      new Master(rpcEnv, SYSTEM_NAME, 6073, conf, true, arguements.isLocal))
+      new Master(rpcEnv, arguments.host, conf, true, arguments.isLocal))
     rpcEnv.awaitTermination()
   //   logInfo(conf.getInt("scache.memory", 1).toString)
   //   logInfo(conf.getString("scache.master", "localhost").toString)
   //   logInfo(conf.getBoolean("scache.boolean", false).toString)
   }
+}
+
+private[deploy] class ClientInfo(val id: Int, val host: String, val port: Int, val ref: RpcEndpointRef) extends Serializable {
+
 }
